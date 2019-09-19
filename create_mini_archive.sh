ad9afd2acf3a278797fa94281f778cd62b8d4a69
Andreas Unterweger <dustsigns@gmail.com>
2019-09-19 13:43:12 +0200

    Add notice in mini archive for missing screenshots

--- create_mini_archive.sh
+++ create_mini_archive.sh
@@ -1,9 +1,8 @@
 #Utility to create a clean compressed archive from this directory with essential files only
-# Andreas Unterweger, 2018
+# Andreas Unterweger, 2018-2019
 #This code is licensed under the 3-Clause BSD License. See LICENSE file for details.
 
 name=$(basename "$PWD")
 make clean > /dev/null
 cd ..
-#TODO: Add hint for missing screenshots folder
-(find "$name" -type f | grep -v '^'$name'\($\|/\(.git\|.gitignore\|screenshots\|testdata\|[^/]\+.sh\)\)' && find "$name/testdata/" -iname '*.txt') | tar czf "${name}_mini.tar.gz" --files-from -
+(find "$name" -type f | grep -v '^'$name'\($\|/\(.git\|.gitignore\|screenshots\|testdata\|[^/]\+.sh\)\)' && find "$name/screenshots/" -iname '*.txt' && find "$name/testdata/" -iname '*.txt') | tar czf "${name}_mini.tar.gz" --files-from -
