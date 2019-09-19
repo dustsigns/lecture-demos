6f619304d1dd5c89c7819086353e494595ec6037
Andreas Unterweger <dustsigns@gmail.com>
2019-09-19 13:49:48 +0200

    Added item count for to-do lists

--- todo.sh
+++ todo.sh
@@ -1,8 +1,8 @@
 #Utility to list all TODOs in sources, headers, scripts and Makefiles
-# Andreas Unterweger, 2017-2018
+# Andreas Unterweger, 2017-2019
 #This code is licensed under the 3-Clause BSD License. See LICENSE file for details.
 
 echo "To-dos in source and header files:"
-find . -iname '*.[ch]pp' -print | xargs grep -o --with-filename --line-number --color=always -e '/[/*]TODO: .*'  | sed 's/\/[/*]TODO: \(.*\)/\1/' | sed 's/\*\/$//'
+find . -iname '*.[ch]pp' -print | xargs grep -o --with-filename --line-number --color=always -e '/[/*]TODO: .*'  | sed 's/\/[/*]TODO: \(.*\)/\1/' | sed 's/\*\/$//' | tee >(wc -l)
 echo "To-dos in script and Make files:"
-find . -iname 'Makefile' -print -o -iname '*.mak' -print -o -iname '*.sh' -print | grep -v ./todo.sh | xargs grep -o --with-filename --line-number --color=always -e '#TODO: .*'  | sed 's/#TODO: \(.*\)/\1/'
+find . -iname 'Makefile' -print -o -iname '*.mak' -print -o -iname '*.sh' -print | grep -v ./todo.sh | xargs grep -o --with-filename --line-number --color=always -e '#TODO: .*'  | sed 's/#TODO: \(.*\)/\1/' | tee >(wc -l)
