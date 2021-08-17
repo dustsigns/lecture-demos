6e45425e2de434df125385a211cad002bff563c9
Andreas Unterweger <dustsigns@gmail.com>
2021-08-17 15:45:03 +0200

    Update to-do script

--- todo.sh
+++ todo.sh
@@ -1,8 +1,13 @@
 #Utility to list all TODOs in sources, headers, scripts and Makefiles
-# Andreas Unterweger, 2017-2019
+# Andreas Unterweger, 2017-2021
 #This code is licensed under the 3-Clause BSD License. See LICENSE file for details.
 
 echo "To-dos in source and header files:"
-find . -iname '*.[ch]pp' -print | xargs grep -o --with-filename --line-number --color=always -e '/[/*]TODO: .*'  | sed 's/\/[/*]TODO: \(.*\)/\1/' | sed 's/\*\/$//' | tee >(wc -l)
+find . -iname '*.[ch]pp' -print | xargs grep -o --with-filename --line-number --color=always -e '/[/*]TODO: .*'  | sed 's/\/[/*]TODO: \(.*\)/\1/' | sed 's/\*\/$//' | tee >(wc -l) &
+wait $!
 echo "To-dos in script and Make files:"
-find . -iname 'Makefile' -print -o -iname '*.mak' -print -o -iname '*.sh' -print | grep -v ./todo.sh | xargs grep -o --with-filename --line-number --color=always -e '#TODO: .*'  | sed 's/#TODO: \(.*\)/\1/' | tee >(wc -l)
+find . -iname 'Makefile' -print -o -iname '*.mak' -print -o -iname '*.sh' -print | grep -v ./todo.sh | xargs grep -o --with-filename --line-number --color=always -e '#TODO: .*'  | sed 's/#TODO: \(.*\)/\1/' | tee >(wc -l) &
+wait $!
+echo "Global to-dos:"
+cat global_todos.txt | tee >(wc -l) &
+wait $!
